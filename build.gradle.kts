f6a6ed0955476205d2d510666bc72d79445afa64
KatanaMajesty <sasha.panasenk@gmail.com>
2022-06-27 15:48:16 +0300

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    java
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    id("io.papermc.paperweight.patcher") version "1.3.7"
+}
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+}
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+subprojects {
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+    }
+}
+
+val spigotDecompiler: Configuration by configurations.creating
+
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content {
+            onlyForConfigurations(
+                configurations.paperclip.name,
+            )
+        }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.605.7")
+    paperclip("io.papermc:paperclip:3.0.2")
+}
+
+paperweight {
+    serverProject.set(project(":bloom-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
+    usePaperUpstream(providers.gradleProperty("paperRef")) {
+        withPaperPatcher {
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+
+            apiOutputDir.set(layout.projectDirectory.dir("bloom-api")) // Bloom
+            serverOutputDir.set(layout.projectDirectory.dir("bloom-server")) // Bloom
+        }
+    }
+}
